53e6e85c0351b4e14ca567704d408feed63369fb
LeeWxx <dnckd0903@naver.com>
2025-07-03 00:49:55 +0900

    Chore: git 커밋 메세지 규칙 설정 추가

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'Feat',    // 기능 추가
+        'Fix',     // 버그 수정
+        'Design',  // 디자인 변경
+        'Style',   // 스타일 변경
+        'Refactor',// 코드 리팩토링
+        'Comment', // 주석 추가 및 변경
+        'Docs',    // 문서 수정
+        'Test',    // 테스트 코드
+        'Chore',   // 빌드 업무 수정, 패키지 매니저 수정
+        'Rename',  // 파일 혹은 폴더명 수정
+        'Remove',  // 파일 삭제
+      ],
+    ],
+    'type-case': [2, 'always', 'pascal-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+}; 
