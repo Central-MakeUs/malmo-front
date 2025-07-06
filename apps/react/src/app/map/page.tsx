a6c544b6c51464b1ccb11dc22bf774d8d79308c3
LeeWxx <dnckd0903@naver.com>
2025-07-06 22:17:48 +0900

    Remove: 사용하지 않는 페이지 제거 (MM-27)

--- apps/react/src/app/map/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/map/')({
-  component: Map,
-})
-
-function Map() {
-  return <div className="m-auto text-2xl font-bold">지도</div>
-}
