8c025899e6ee1533c5bd8a47b4679a715961df2a
hin6150 <hin6150@gmail.com>
2025-07-20 07:55:35 +0900

    Design: 단어 단위로 줄바꿈 처리 (MM-35)

--- apps/react/src/features/chat-result/ui/chat-result-main-info.tsx
+++ apps/react/src/features/chat-result/ui/chat-result-main-info.tsx
@@ -11,15 +11,16 @@ export function ChatResultMainInfo({ date, subject, onViewChat }: ChatResultMain
     <div className="flex items-center gap-6">
       <div className="flex-1">
         <p className="body4-medium mb-1 text-gray-iron-500">{date}</p>
-        <h1 className="heading1-bold">{subject}</h1>
+        <h1 className="heading1-bold break-keep">{subject}</h1>
       </div>
-      <div
+      <button
+        type="button"
         className="flex h-fit cursor-pointer items-center rounded-[8px] bg-gray-iron-700 py-2 pr-3 pl-[18px] text-gray-iron-25"
         onClick={onViewChat}
       >
         <p className="body3-medium">대화보기</p>
         <ChevronRight className="h-4 w-4" />
-      </div>
+      </button>
     </div>
   )
 }
