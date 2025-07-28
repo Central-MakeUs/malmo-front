87120f2c969ec48b8cdc7329328e38ab1076d37a
hin6150 <hin6150@gmail.com>
2025-07-28 17:01:34 +0900

    Feat: api 서비스단 업데이트 (MM-44)

--- apps/react/src/shared/services/question.service.ts
+++ apps/react/src/shared/services/question.service.ts
@@ -1,5 +1,5 @@
 import apiInstance from '../libs/api'
-import { QuestionsApi } from '@data/user-api-axios/api'
+import { AnswerRequestDto, QuestionsApi } from '@data/user-api-axios/api'
 
 class QuestionService extends QuestionsApi {
   constructor() {
@@ -7,7 +7,31 @@ class QuestionService extends QuestionsApi {
   }
 
   async fetchTodayQuestion() {
-    const { data } = await super.getTodayQuestion()
+    const { data } = await this.getTodayQuestion()
+    return data
+  }
+
+  async fetchQuestionList(level: number) {
+    const { data } = await this.getQuestion({ level: 0 })
+    return data
+  }
+
+  async fetchPastQuestion(questionId: number) {
+    const { data } = await this.getAnswers({ coupleQuestionId: questionId })
+    return data
+  }
+
+  async postTodayQuestionAnswer(body: AnswerRequestDto) {
+    const { data } = await this.postAnswer({
+      answerRequestDto: { ...body },
+    })
+    return data
+  }
+
+  async patchTodayQuestionAnswer(body: AnswerRequestDto) {
+    const { data } = await this.updateAnswer({
+      answerRequestDto: { ...body },
+    })
     return data
   }
 }
