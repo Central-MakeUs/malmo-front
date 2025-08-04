b15c6d0ea38fd41643ed45e8b92d36b67cbc2a7f
LeeWxx <dnckd0903@naver.com>
2025-08-05 02:46:29 +0900

    Design: 커플 연결 끊기 모달 줄넘김 추가 (MM-63)

--- apps/react/src/features/profile/hooks/use-profile-modal.tsx
+++ apps/react/src/features/profile/hooks/use-profile-modal.tsx
@@ -90,7 +90,11 @@ export function useProfileModal(): UseProfileModalReturn {
   const coupleDisconnectModal = (onSuccess?: () => void) => {
     alertDialog.open({
       title: '정말 커플 연동을 끊으시겠어요?',
-      description: '커플 연결을 끊으면 데이터가 모두 삭제돼요.30일 이내로 다시 연동하면 복구할 수 있어요.',
+      description: (
+        <p>
+          커플 연결을 끊으면 데이터가 모두 삭제돼요. <br /> 30일 이내로 다시 연동하면 복구할 수 있어요.
+        </p>
+      ),
       cancelText: '연결 끊기',
       confirmText: '취소하기',
       onCancel: () => {
