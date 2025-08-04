9d46c9732a5a6a4af10e76fdb790ec2634534b03
LeeWxx <dnckd0903@naver.com>
2025-08-04 20:11:55 +0900

    Design: 대화 요약 디자인 수정 (MM-63)

--- apps/react/src/app/chat/result/page.tsx
+++ apps/react/src/app/chat/result/page.tsx
@@ -68,16 +68,16 @@ function RouteComponent() {
     )
 
   return (
-    <div className="flex h-full flex-col pt-[50px]">
+    <div className="flex h-full flex-col bg-white pt-[50px]">
       <DetailHeaderBar right={exitButton()} showBackButton={fromHistory} className="fixed top-0 bg-malmo-rasberry-25" />
 
-      <div className="flex-1 bg-malmo-rasberry-25 pt-3">
+      <div className="flex flex-col bg-malmo-rasberry-25 pt-3">
         <ChatResultHeader
           title="대화 요약이 완료되었어요!"
           description={'모모가 고민을 해결하는 데<br /> 도움을 주었길 바라요'}
         />
 
-        <div className="flex flex-col gap-7 rounded-t-[24px] bg-white px-5 py-10">
+        <div className="rounded-t-[24px] bg-white px-5 py-10">
           {!chatResult ? (
             <div className="flex flex-1 items-center justify-center bg-white">
               <p className="body1-regular text-gray-500">요약 결과를 불러오는 중입니다...</p>
@@ -90,17 +90,17 @@ function RouteComponent() {
             />
           )}
 
-          <hr className="border-gray-100" />
+          <hr className="mt-7 border-gray-100" />
 
-          {summaryData.map(({ title, content }) => (
-            <ChatResultSummarySection
-              key={title}
-              title={title}
-              content={content ? content : '진행하지 않은 단계에요.'}
-            />
-          ))}
-
-          <Button className="mt-13" text="홈으로 이동하기" onClick={() => navigate({ to: '/' })} />
+          <div className="mt-7 flex flex-col gap-9">
+            {summaryData.map(({ title, content }) => (
+              <ChatResultSummarySection
+                key={title}
+                title={title}
+                content={content ? content : '진행하지 않은 단계에요.'}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
