c7811804f3c4945f1cfbd9e7e3e2845529288760
hin6150 <hin6150@gmail.com>
2025-08-19 08:42:43 +0900

    Design: 짧은 환경에서도 dash 강제 (MM-71)

--- apps/react/src/features/chat/hooks/use-chatting-modal.tsx
+++ apps/react/src/features/chat/hooks/use-chatting-modal.tsx
@@ -137,8 +137,8 @@ export function useChattingModal(chatStatus?: ChatRoomStateDataChatRoomStateEnum
             </div>
 
             <div className="relative">
-              <div className="absolute top-[-25px] left-0 h-[24px] border-l-2 border-dashed border-l-malmo-rasberry-400">
-                <div className="absolute bottom-0 left-[-5px] h-2 w-2 rounded-full bg-malmo-rasberry-300" />
+              <div className="absolute top-[-25px] left-0 h-[24px] w-[2px] bg-[repeating-linear-gradient(to_bottom,#ec5c7a_0_4px,transparent_4px_8px)]">
+                <div className="absolute bottom-0 left-[-3.5px] h-2 w-2 rounded-full bg-malmo-rasberry-300" />
               </div>
               <p className="mt-1">
                 2. 종료 후에는 모모와 나눈 <span className={highlightedText}>대화를 요약</span>해서 보여줘요.
